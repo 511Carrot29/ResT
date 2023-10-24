ef238ea4871b72d49a795ce5b3a5a4d8ec80d799
511Carrot29 <93975005+511Carrot29@users.noreply.github.com>
2023-10-24 10:28:09 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,7 +18,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributed as dist
-from torch._six import inf
 
 from torch.utils.tensorboard import SummaryWriter
 
